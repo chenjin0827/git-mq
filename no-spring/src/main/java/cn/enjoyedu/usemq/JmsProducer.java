3d67d956ff9be269e42fb640ba7fd1bfad776d36
陈进 <1219650953@qq.com>
2020-11-11 17:44:53 +0800

    1

--- no-spring/src/main/java/cn/enjoyedu/usemq/JmsProducer.java
+++ no-spring/src/main/java/cn/enjoyedu/usemq/JmsProducer.java
@@ -6,7 +6,7 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import javax.jms.*;
 
 /**
- *类说明：生产者端
+ * 类说明：生产者端
  */
 public class JmsProducer {
 
@@ -19,57 +19,56 @@ public class JmsProducer {
     /* 默认连接地址*/
     private static final String BROKEURL
             = ActiveMQConnection.DEFAULT_BROKER_URL;
+    /*发送数量*/
     private static final int SENDNUM = 3;
 
     public static void main(String[] args) {
-        /* 连接工厂*/
-        ConnectionFactory connectionFactory;
-        /* 连接*/
-        Connection connection = null;
-        /* 会话*/
-        Session session;
-        /* 消息的目的地*/
-        Destination destination;
-        /* 消息的生产者*/
-        MessageProducer messageProducer;
 
-        /* 实例化连接工厂*/
-        connectionFactory = new ActiveMQConnectionFactory(USERNAME,PASSWORD,
+
+        /*连接工厂*/
+        ConnectionFactory connectionFactory;
+        /*连接*/
+        Connection connection = null;
+        /*会话*/
+        Session session;
+        /*消息目的地*/
+        Destination destination;
+        /*消息的生产者*/
+        MessageProducer messageProducer;
+        /*实例化连接工厂*/
+        connectionFactory = new ActiveMQConnectionFactory(USERNAME, PASSWORD,
                 BROKEURL);
         try {
-            /* 通过连接工厂获取连接*/
+        /*通过连接工厂获取连接*/
             connection = connectionFactory.createConnection();
-            /* 启动连接*/
+        /*启动连接*/
             connection.start();
-            /* 创建session
+        /* 创建session
             * 第一个参数表示是否使用事务，第二次参数表示是否自动确认*/
-            session = connection.createSession(false,
-                    Session.AUTO_ACKNOWLEDGE);
-            /* 创建一个名为HelloWorld消息队列*/
-            //destination = session.createTopic("HelloActiveMq");
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        /* 创建一个名为HelloActiveMqQueue消息队列*/
             destination = session.createQueue("HelloActiveMqQueue");
-            /* 创建消息生产者*/
+        /*创建消息生产者*/
             messageProducer = session.createProducer(destination);
-            /* 循环发送消息*/
-            for(int i=0;i<SENDNUM;i++){
-                String msg = "发送消息"+i+" "+System.currentTimeMillis();
+        /*循环发送消息*/
+            for (int i = 0; i < SENDNUM; i++) {
+                String msg = "发送消息" + i + " " + System.currentTimeMillis();
                 TextMessage textMessage = session.createTextMessage(msg);
-                System.out.println("标准用法:"+msg);
+                System.out.println("标准用法:" + msg);
                 messageProducer.send(textMessage);
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }finally {
-            if(connection!=null){
+        } finally {
+            if (connection != null) {
                 try {
                     connection.close();
                 } catch (JMSException e) {
                     e.printStackTrace();
                 }
             }
-
         }
+
+
     }
-
-
 }
